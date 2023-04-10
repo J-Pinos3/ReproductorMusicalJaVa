16cb8c49134b21e45a2e47e405b43370304bba3c
J-Pinos3 <josloco665@gmail.com>
2023-04-10 09:36:59 -0500

    Cambios en la BD se almacenará las rtuas de los archivos

--- reproductormp3.sql
+++ reproductormp3.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS album
 (
 	id_album int auto_increment primary key,
     nombre_album varchar(50) not null,
-    foto_album longblob not null
+    foto_album varchar(200) not null
 );
 
 CREATE TABLE IF NOT EXISTS cancion
@@ -15,5 +15,10 @@ CREATE TABLE IF NOT EXISTS cancion
     autor_cancion varchar(70) not null,
     genero_cancion varchar(70) not null,
     anio_cancion date not null,
-    song longblob not null
+    song_file varchar(200) not null,
+    
+    id_album int,
+    foreign key (id_album) references album(id_album) on delete cascade
 );
+
+SELECT * FROM cancion;
