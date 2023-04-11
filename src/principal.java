5f0393bf9b3a673f9838c4fbc6d003d6ce07efdf
J-Pinos3 <josloco665@gmail.com>
2023-04-10 21:11:14 -0500

    Pestaña principal V1

---
+++ src/principal.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class principal {
+    private JPanel principal_panel;
+    private JTextField buscarCanciónArtistaOTextField;
+    private JButton buscarCanciónButton;
+    private JButton buscarÁlbumButton;
+    private JButton buscarArtistaButton;
+    private JButton CANCIONESButton;
+    private JButton ÁLBUMSButton;
+}
