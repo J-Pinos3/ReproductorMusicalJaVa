e4b4032d64354abaa3df1ee4ad5d2e68f6bef786
J-Pinos3 <josloco665@gmail.com>
2023-04-12 20:14:22 -0500

    Pestaña Apara trabajar con los álbumes

--- src/Vista/vistaAlbumes.java
+++ src/Vista/vistaAlbumes.java
@@ -1,4 +1,15 @@
 package Vista;
 
+import javax.swing.*;
+
 public class vistaAlbumes {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton ARCHIVOSButton;
+    private JTable table1;
+    private JButton agregarÁlbumButton;
+    private JButton buscarÁlbumButton;
+    private JButton modificarÁlbumButton;
+    private JButton eliminarÁlbumButton;
+    private JButton listarButton;
 }
