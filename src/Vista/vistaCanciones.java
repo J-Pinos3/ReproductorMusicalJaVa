8e49f1667841c53cad971da472064b14fe2ba40a
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2023-04-12 21:12:06 -0500

    Pestaña canciones

--- src/Vista/vistaCanciones.java
+++ src/Vista/vistaCanciones.java
@@ -1,4 +1,16 @@
 package Vista;
 
+import javax.swing.*;
+
 public class vistaCanciones {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTable table1;
+    private JButton agregarCanciónButton;
+    private JButton buscarCanciónButton;
+    private JButton modificarCanciónButton;
+    private JButton eliminarCanciónButton;
+    private JButton listarButton;
+    private JComboBox comboBox1;
+    private JTextField textField3;
 }
