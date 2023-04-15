708008cd916eb7d3afa48b878a4410c37f30f7ac
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2023-04-15 17:18:21 -0500

    Regustrar Cancion y Elimiar cancion

--- src/Modelo/CancionDAO.java
+++ src/Modelo/CancionDAO.java
@@ -1,11 +1,12 @@
 package Modelo;
 import javax.swing.*;
 import java.awt.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
 
-public class CancionDAO {
+public class CancionDAO implements Serializable {
     Conexion cnn = new Conexion();
     Connection con;
     PreparedStatement ps;
@@ -45,8 +46,33 @@ public class CancionDAO {
         }
 
     }//FIN DEL MÉTODO REGISTRARCANCION
+    public boolean EliminarCancion(String titulo_cancion, String nombre_album){
+        String sql= "DELETE FROM cancion WHERE titulo_cancion=? AND id_album=(SELECT id_album FROM album WHERE nombre_album=?)";
+        try{
+            con = cnn.getConnection();
+            ps= con.prepareStatement(sql);
+            ps.setString(1, titulo_cancion);
+            ps.setString(2, nombre_album);
 
+            int res=ps.executeUpdate();
+            if(res > 0){
+                JOptionPane.showMessageDialog(null,"Cancion eliminada exitosamente");
+            }else{
+                JOptionPane.showMessageDialog(null,"Error al eliminar la canción");
+            }
 
+            return true;
+        }catch (HeadlessException | SQLException e){
+            JOptionPane.showMessageDialog(null,"Error -> cancionDAO: " + e.toString());
+            return false;
+        }finally {
+            try {
+                con.close();
+            }catch (SQLException e1){
+                System.out.println("Error: cancionDAO: " + e1.toString());
+            }
+        }
+    }//FIN DEL MÉTODO ELIMINARCANCION
 
 
 }
