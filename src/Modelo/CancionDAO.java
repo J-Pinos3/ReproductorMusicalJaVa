c9c4814a958ceba09472ed3d00a891001ed7df83
J-Pinos3 <josloco665@gmail.com>
2023-04-26 20:10:00 -0500

    Funciones en Canciones dao y se empieza a usar la interfaz de las canciones

--- src/Modelo/CancionDAO.java
+++ src/Modelo/CancionDAO.java
@@ -24,7 +24,7 @@ public class CancionDAO implements Serializable {
             ps.setString(3, can.getGenero());
             ps.setString(4, can.getAnio());
             ps.setString(5, can.getCancionFile());
-            ps.setInt(1, can.getAlbumId());
+            ps.setInt(6, can.getAlbumId());
 
             int res = ps.executeUpdate();
             if(res > 0){
@@ -46,6 +46,8 @@ public class CancionDAO implements Serializable {
         }
 
     }//FIN DEL MÉTODO REGISTRARCANCION
+
+
     public boolean EliminarCancion(String titulo_cancion, String nombre_album){
         String sql= "DELETE FROM cancion WHERE titulo_cancion=? AND id_album=(SELECT id_album FROM album WHERE nombre_album=?)";
         try{
@@ -54,13 +56,7 @@ public class CancionDAO implements Serializable {
             ps.setString(1, titulo_cancion);
             ps.setString(2, nombre_album);
 
-            int res=ps.executeUpdate();
-            if(res > 0){
-                JOptionPane.showMessageDialog(null,"Cancion eliminada exitosamente");
-            }else{
-                JOptionPane.showMessageDialog(null,"Error al eliminar la canción");
-            }
-
+            ps.execute();
             return true;
         }catch (HeadlessException | SQLException e){
             JOptionPane.showMessageDialog(null,"Error -> cancionDAO: " + e.toString());
@@ -73,23 +69,23 @@ public class CancionDAO implements Serializable {
             }
         }
     }//FIN DEL MÉTODO ELIMINARCANCION
-    public boolean ActualizarCancion(Cancion can, String nombre_album, String titulo_cancion){
-        String sql="UPDATE cancion SET titulo_cancion=?, autor_cancion=?, genero_cancion=?, anio_cancion=?, song_file=? WHERE id_album=(SELECT id_album FROM album WHERE nombre_album=?) AND titulo_cancion=?";
+
+
+    public boolean ActualizarCancion(Cancion can){
+        String sql="UPDATE cancion SET titulo_cancion=?, autor_cancion=?, genero_cancion=?, anio_cancion=?, song_file=? , id_alnum = ? " +
+                "WHERE id_cancion = ?";
         try {
             con = cnn.getConnection();
             ps= con.prepareStatement(sql);
-            ps.setString(1, can.getAutor());
-            ps.setString(2, can.getGenero());
-            ps.setString(3, can.getAnio());
-            ps.setString(4, can.getCancionFile());
-            ps.setString(5, nombre_album);
-            ps.setString(6, titulo_cancion);
-            int res = ps.executeUpdate();
-            if(res > 0){
-                JOptionPane.showMessageDialog(null,"Canción actualizada exitosamente");
-            }else{
-                JOptionPane.showMessageDialog(null,"Error al actualizar la canción");
-            }
+            ps.setString(1, can.getTitulo());
+            ps.setString(2, can.getAutor());
+            ps.setString(3, can.getGenero());
+            ps.setString(4, can.getAnio());
+            ps.setString(5, can.getCancionFile());
+            ps.setInt(6, can.getAlbumId());
+            ps.setInt(7,can.getId());
+            ps.execute();
+
             return true;
         }catch (HeadlessException | SQLException e){
             JOptionPane.showMessageDialog(null,"Error -> cancionDAO: " + e.toString());
@@ -103,24 +99,16 @@ public class CancionDAO implements Serializable {
         }
     }//FIN DEL MÉTODO ACTUALIZARCANCION
 
-    public List<Cancion> BuscarCancion(String nombre_album, String busqueda){
-        String sql = "SELECT c.id_cancion, c.titulo_cancion, c.autor_cancion, c.genero_cancion, c.anio_cancion, c.song_file, a.id_album " +
-                "FROM cancion c " +
-                "JOIN album a ON c.id_album = a.id_album " +
-                "WHERE a.nombre_album = ? ";
-        if (busqueda != null && !busqueda.isEmpty()) {
-            sql += "AND c.titulo_cancion LIKE ?";
-        }
 
+    public List ListarCancion(){
+        String sql = "SELECT * FROM cancion ";
+        List<Cancion> canciones = new ArrayList<>();
         try {
             con = cnn.getConnection();
             ps = con.prepareStatement(sql);
-            ps.setString(1, nombre_album);
-            if (busqueda != null && !busqueda.isEmpty()) {
-                ps.setString(2, "%" + busqueda + "%");
-            }
             rs = ps.executeQuery();
-            List<Cancion> canciones = new ArrayList<>();
+
+
             while (rs.next()) {
                 Cancion can = new Cancion();
                 can.setId(rs.getInt("id_cancion"));
@@ -135,13 +123,10 @@ public class CancionDAO implements Serializable {
             return canciones;
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Error -> cancionDAO: " + e.toString());
-            return null;
-        } finally {
-            try {
-                con.close();
-            } catch (SQLException e1) {
-                System.out.println("Error: cancionDAO: " + e1.toString());
-            }
-        }//FIN DEL MÉTODO LISTAR,BUSCARCANCION
-    }
+
+        }
+        return canciones;
+    }//FIN DEL MÉTODO LISTARCANCION
+
+
 }
--- src/Vista/vistaCanciones.java
+++ src/Vista/vistaCanciones.java
@@ -1,16 +1,116 @@
 package Vista;
 
-import javax.swing.*;
+import Modelo.Cancion;
+import Modelo.CancionDAO;
+import Modelo.Conexion;
 
-public class vistaCanciones {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTable table1;
-    private JButton agregarCanciónButton;
-    private JButton buscarCanciónButton;
-    private JButton modificarCanciónButton;
-    private JButton eliminarCanciónButton;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class vistaCanciones extends JFrame{
+    private JTextField txtTitulo;
+    private JTextField rutaMp3;
+    private JTable tablaSongs;
+    private JButton agregarCancionButton;
+    private JButton buscarCancionButton;
+    private JButton modificarCancionButton;
+    private JButton eliminarCancionButton;
     private JButton listarButton;
-    private JComboBox comboBox1;
-    private JTextField textField3;
-}
+    private JComboBox cmbAlbum;
+    private JTextField rutaFoto;
+    private JTextField txtGenero;
+    private JTextField txtLanzamiento;
+    private JTextField txtArtista;
+    private JPanel panelCancionesCrud;
+
+    Cancion can = new Cancion();
+    CancionDAO cancionDao = new CancionDAO();
+
+    public vistaCanciones() {
+
+        rellenarAlbumes();
+
+        setContentPane(panelCancionesCrud);
+        setVisible(true);
+        setTitle("GESTIONAR CANCIONES");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(900,700);
+
+        agregarCancionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!"".equals(txtTitulo.getText())){
+
+                }
+            }
+        });
+
+        /*-------------------------------------------------------------------*/
+        eliminarCancionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        /*-------------------------------------------------------------------*/
+
+        modificarCancionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        /*-------------------------------------------------------------------*/
+
+
+        buscarCancionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        /*-------------------------------------------------------------------*/
+
+
+        listarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }//CONSTRUCTOR
+
+
+
+    public void rellenarAlbumes(){
+        Connection con;
+        PreparedStatement ps;
+        ResultSet rs;
+        Conexion cnn = new Conexion();
+        con = cnn.getConnection();
+        String sql = "SELECT nombre_album from album";
+
+        try{
+            ps=con.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                cmbAlbum.addItem(rs.getString("nombre_album"));
+            }
+
+            ps.close();
+            rs.close();
+        }catch (HeadlessException | SQLException E1){
+            JOptionPane.showMessageDialog(null,"No se pudieron cargar los álbumes en el combo box");
+        }
+    }
+
+}//FIN DE LA CLASE VISTACANCIONES
+
